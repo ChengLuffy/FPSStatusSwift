024809d8f6ba9b8cc93d4f4d8cc2652f5ade2cd7
ChengLuffy <chengluffy@hotmail.com>
2016-07-30 09:04:19 +0800

    Initial Commit

---
+++ FPSStatusSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FPSStatusSwift
+//
+//  Created by 成璐飞 on 16/7/30.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

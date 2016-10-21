ddf60071ea7edb7cff06cca4c01e021874d1c3aa
ChengLuffy <chengluffy@hotmail.com>
2016-10-21 16:27:36 +0800

    carthage

---
+++ FPSStatusSwift/FPSStatusSwift.h
@@ -0,0 +1,19 @@
+//
+//  FPSStatusSwift.h
+//  FPSStatusSwift
+//
+//  Created by 成璐飞 on 2016/10/21.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FPSStatusSwift.
+FOUNDATION_EXPORT double FPSStatusSwiftVersionNumber;
+
+//! Project version string for FPSStatusSwift.
+FOUNDATION_EXPORT const unsigned char FPSStatusSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPSStatusSwift/PublicHeader.h>
+
+

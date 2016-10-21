cd0a30b07a65f762dbeabfa00efe3d80b3118f3d
ChengLuffy <chengluffy@hotmail.com>
2016-10-21 15:33:26 +0800

    carthage

---
+++ FPSStatus/FPSStatus.h
@@ -0,0 +1,19 @@
+//
+//  FPSStatus.h
+//  FPSStatus
+//
+//  Created by 成璐飞 on 2016/10/21.
+//  Copyright © 2016年 成璐飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FPSStatus.
+FOUNDATION_EXPORT double FPSStatusVersionNumber;
+
+//! Project version string for FPSStatus.
+FOUNDATION_EXPORT const unsigned char FPSStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPSStatus/PublicHeader.h>
+
+
